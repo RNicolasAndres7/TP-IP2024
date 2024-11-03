616308a6f570921c837dea6f1d3a4e84bfdb2a72
Jesica Leiva <jesssleiva@gmail.com>
2024-11-03 16:35:53 -0300

    funcionando inicio de sesion

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
